2545cf53b36e2e5d7b75f901d33fca4424822999
Hamlet zheng <1025157436@qq.com>
2018-01-10 13:50:22 +0800

    bayes : add files

---
+++ bayes.py
@@ -0,0 +1,109 @@
+#coding=utf-8
+# Example of Naive Bayes implemented from Scratch in Python
+import csv
+import random
+import math
+
+#load data from file
+def load_csv(filename):
+    lines = csv.reader(open(filename, "rb"))
+    dataset = list(lines)
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+#split dataset
+def split_dataset(dataset, split_ratio):
+    train_size = int(len(dataset) * split_ratio)
+    train_set = []
+    copy = list(dataset)
+    while len(train_set) < train_size:
+        index = random.randrange(len(copy))
+        train_set.append(copy.pop(index))
+    return [train_set, copy]
+
+#separate data
+def separate_by_class(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if(vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+#calculate avg
+def mean(numbers):
+    return sum(numbers)/float(len(numbers))
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x-avg, 2) for x in numbers]) / float(len(numbers)-1)
+    return math.sqrt(variance)
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = {}
+    for class_value, instances in separated.iteritems():
+        summaries[class_value] = summarize(instances)
+    return summaries
+
+#probability
+def calculate_probability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+
+def calculate_class_probabilities(summaries, input_vector):
+    probabilities = {}
+    for class_value, class_summaries in summaries.iteritems():
+        probabilities[class_value] = 1
+        for i in range(len(class_summaries)):
+            mean, stdev = class_summaries[i]
+            x = input_vector[i]
+            probabilities[class_value] *= calculate_probability(x, mean, stdev)
+    return probabilities
+
+def predict(summaries, input_vector):
+    probabilities = calculate_class_probabilities(summaries, input_vector)
+    best_label, best_prob = None, -1
+    for class_value, probability in probabilities.iteritems():
+        if best_label is None or probability > best_prob:
+            best_label = class_value
+            best_prob = probability
+    return best_label
+
+def get_predictions(summaries, test_set):
+    predictions = []
+    for i in range(len(test_set)):
+        result = predict(summaries, test_set[i])
+        predictions.append(result)
+    return predictions
+
+def get_accuracy(test_set, predictions):
+    correct = 0
+    for x in range(len(test_set)):
+        if test_set[x][-1] == predictions[x]:
+            correct += 1
+    return (correct / float(len(test_set))) * 100.0
+
+#main
+def main():
+    filename = 'pima-indians-diabetes.data.csv'
+    split_ratio = 0.67
+    dataset = load_csv(filename)
+    train_set, test_set = split_dataset(dataset, split_ratio)
+    print('Split {0} rows into train={1} and test={2} rows').format(len(dataset), len(train_set), len(test_set))    
+    summaries = summarize_by_class(train_set)
+
+    predictions = get_predictions(summaries, test_set)
+    accuracy = get_accuracy(test_set, predictions)
+    print('accuracy: {0}%').format(accuracy)
+
+
+if __name__ == '__main__':
+    main()
